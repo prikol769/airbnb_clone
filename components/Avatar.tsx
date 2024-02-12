37b2da0021ad1f23f8e60349cf5143ded44d4de9
prikol769 <prikol769@gmail.com>
2024-02-11 19:14:57 -0600

    added Avatar component

---
+++ components/Avatar.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <Image
+      alt="avatar"
+      width={30}
+      height={30}
+      className="rounded-full"
+      src="/images/placeholder.jpg"
+    />
+  );
+};
+
+export default Avatar;
