6a0c71dabd7ad85b429b5def8e6fd27d62374dde
prikol769 <prikol769@gmail.com>
2024-02-15 11:00:22 -0600

    added onToggle func RegisterModal

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -12,9 +12,11 @@ import Input from "../inputs/Inputs";
 import toast from "react-hot-toast";
 import Button from "../Button";
 import { signIn } from "next-auth/react";
+import useLoginModal from "@/hooks/useLoginModal";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -79,7 +81,8 @@ const RegisterModal = () => {
 
   const onToggle = useCallback(() => {
     registerModal.onClose();
-  }, [registerModal]);
+    loginModal.onOpen();
+  }, [loginModal, registerModal]);
 
   const footerContent = (
     <div className="flex flex-col gap-4 mt-3">
