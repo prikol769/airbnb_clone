4b3c60443aeeb0ca2b730cbc810578c9778f7c38
prikol769 <prikol769@gmail.com>
2024-02-21 16:08:27 -0600

    fix RegisterModal after register open loginModal

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -39,6 +39,7 @@ const RegisterModal = () => {
       .then(() => {
         toast.success("Registered!");
         registerModal.onClose();
+        loginModal.onOpen();
       })
       .catch((error) => {
         toast.error("Something went wrong!");
