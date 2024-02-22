2ddeff8d0b855a9492e5d9d6539276349570569a
prikol769 <prikol769@gmail.com>
2024-02-22 10:30:06 -0600

    fix user menu close

--- components/navbar/UserMenu.tsx
+++ components/navbar/UserMenu.tsx
@@ -61,7 +61,7 @@ const UserMenu = ({ currentUser }: UserMenuProps) => {
           className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4
                    bg-white overflow-hidden right-0 top-12 text-sm"
         >
-          <div className="flex flex-col cursor-pointer">
+          <div className="flex flex-col cursor-pointer" onClick={toggleOpen}>
             {currentUser ? (
               <>
                 <MenuItem
